f7f74b1da7dd7f448d7cdf05df27233c5e6f9e1e
Kumar Krishnamoorthy <kumar.softindies@gmail.com>
2022-02-26 20:43:07 +0530

    Wow Ver1.1

--- src/mockAPI.js
+++ src/mockAPI.js
@@ -2,7 +2,6 @@ const API = [
   {
     name: "Kiwi",
     price: 19.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/51312/kiwi-fruit-vitamins-healthy-eating-51312.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -10,7 +9,6 @@ const API = [
   {
     name: "Watermelon",
     price: 14.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/1313267/pexels-photo-1313267.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -18,7 +16,6 @@ const API = [
   {
     name: "Cherry",
     price: 8.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/109274/pexels-photo-109274.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -26,7 +23,6 @@ const API = [
   {
     name: "Avacado",
     price: 6.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/557659/pexels-photo-557659.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -34,7 +30,6 @@ const API = [
   {
     name: "Apple",
     price: 5.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -42,7 +37,6 @@ const API = [
   {
     name: "Strawberry",
     price: 11.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/934066/pexels-photo-934066.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -50,7 +44,6 @@ const API = [
   {
     name: "Orange",
     price: 3.99,
-    quantity: 1,
     inCart: false,
     src:
       "https://images.pexels.com/photos/42059/citrus-diet-food-fresh-42059.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
